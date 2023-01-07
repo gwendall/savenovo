95dbdf26aca8269a0de4abb2cb011547f64c30a2
gwendall <gwendall.esnault@gmail.com>
2023-01-07 02:10:40 +0100

    fix

---
+++ src/utils/contract.ts
@@ -0,0 +1,8 @@
+import saveNovoContractABI from '../../public/abi.json';
+
+export const saveNovoContractAddress = '0xbc3b0ce71b5edd18d4a7d80d3bef1a40211e67ad' as `0x${string}`;
+
+export const saveNovoContract = {
+  address: saveNovoContractAddress,
+  abi: saveNovoContractABI,
+};
