39589c0ff66fd15b1fb6353df978a76f6b4180aa
gwendall <gwendall.esnault@gmail.com>
2023-01-08 02:51:03 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -238,7 +238,7 @@ const Home: NextPage = () => {
       )}
       <Main>
         <Title>Save Novo</Title>
-        <div>Mint novo's punk pixels to help buy his punk back</div>
+        <div>{ `Mint novo's punk pixels to help buy his punk back` }</div>
         <div style={{ marginTop: 35, marginBottom: 40 }}>
           <Punk />
         </div>
