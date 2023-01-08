d9769049424e9ba26955295ee32b6ba0eb9c49d1
gwendall <gwendall.esnault@gmail.com>
2023-01-08 02:43:44 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -299,7 +299,7 @@ const Home: NextPage = () => {
           No royalties, 100% of the proceeds will be sent to a recovery wallet (<ExternalLink href={`https://etherscan.io/address/${recoveryWalletAddress}`}>{shortenAddress(recoveryWalletAddress)}</ExternalLink>) used to get the punk back.<br /><br />
           Happy minting!<br /><br />
           Love,<br />
-          Some punks - <ExternalLink href="https://twitter.com/gwendall">gwendall</ExternalLink> & <ExternalLink href="https://twitter.com/franknft_eth">franknft_eth</ExternalLink>
+          Some punks
         </Description>
         <div>
           <ExternalLink href={ `https://goerli.etherscan.io/address/${saveNovoContractAddress}` }>Contract</ExternalLink>
